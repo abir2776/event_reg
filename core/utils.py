95cdc18686d20a08981fa66dc122f8ca8322178c
abir2776 <osmangoni255@gmail.com>
2023-11-29 22:46:37 +0600

    Created Custom user model

---
+++ core/utils.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def get_user_slug(instance):
+    return (
+        f"{instance.first_name}-{instance.last_name}-{str(instance.uid).split('-')[0]}"
+    )
+
+
+# Media File Prefixes
+def get_user_media_path_prefix(instance, filename):
+    return f"users/{instance.slug}/{filename}"
